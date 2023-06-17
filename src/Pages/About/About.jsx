1f97851f4563a9b85fc817e1fc60515af4e17b11
jordanowens1125 <60613533+jordanowens1125@users.noreply.github.com>
2023-06-16 22:12:58 -0500

    Update About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -7,11 +7,7 @@ const About = () => {
       <div>
         <h2>About Us</h2>
       <p>
-        Sed scelerisque sollicitudin felis eu luctus. Nam auctor consectetur
-        maximus. Proin varius magna elit, vestibulum aliquet augue posuere
-        lobortis. Phasellus viverra dolor risus, sollicitudin hendrerit metus
-        interdum id. Sed ut erat urna. Morbi lobortis gravida purus, quis
-        pretium odio pulvinar eu.{" "}
+        To help build a better world, where every person is free to move and pursue their dreams. We believe in the power of creating a world with fewer obstacles and limits, where people have the freedom to build a better life and pursue their dreams. To shorten the distance between where you are and where you want to go.
       </p>
       </div>
       
